9f2cec4e1a5760f78dda5eaeeff8c4c0ce1f1b1b
Colin Fraser <colin.fraser@chimp.net>
2018-03-08 16:52:33 -0800

    problem 1 first try

--- TopTitlesMapper.py
+++ TopTitlesMapper.py
@@ -5,5 +5,5 @@ test = False
 input_data = sys.stdin if not test else ["A 100", "C 98", "B 99", "E 96", "D 97", "G 90", "F 96"]
 
 for line in input_data:
-    sys.stdout.write(line + '\n')
+    sys.stdout.write(line)
 
