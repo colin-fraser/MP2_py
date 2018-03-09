cde2f8d1248960c72ea507efa2381cb4b7d62aa3
Colin Fraser <colin.fraser@chimp.net>
2018-03-09 11:50:28 -0800

    eliminate runtime errors by adding pass statements

--- TopTitleStatisticsMapper.py
+++ TopTitleStatisticsMapper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
+test = False
 
+input_data = sys.stdin if not test else ["A 100", "C 98", "B 99", "E 96", "D 97", "G 90", "F 96"]
 
-for line in sys.stdin:
-    # TODO
-    pass
+for line in input_data:
+    sys.stdout.write(line)
+
--- TopTitleStatisticsReducer.py
+++ TopTitleStatisticsReducer.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
 import sys
 
+test = True
 
+total_sum = 0
+maximum = 0
+minimum = 10e10
+n = 0
+counts = list()
+sq_err = 0
 
-#TODO
+input_data = sys.stdin if not test else ["A\t100\n", "C\t98\n", "B\t99\n", "E\t96\n", "D\t97\n", "G\t90\n", "F\t96\n"]
+for line in input_data:
+    word, count = line.split('\t')
+    count = int(count)
 
-for line in sys.stdin:
-    pass
+    total_sum += count
+    if count > maximum:
+        maximum = count
+    if count < minimum:
+        minimum = count
+    n += 1
 
-#TODO
+mean = total_sum / n
+
+for line in input_data:
+    word, count = line.split('\t')
+    count = int(count)
+
+    sq_err += (count - mean)**2
+
+var = sq_err / n
+
+sys.stdout.write('Mean\t%i\n' % mean)
+sys.stdout.write('Sum\t%i\n' % total_sum)
+sys.stdout.write('Min\t%i\n' % minimum)
+sys.stdout.write('Max\t%i\n' % maximum)
+sys.stdout.write('Var\t%i' % var)
